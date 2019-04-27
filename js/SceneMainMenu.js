c2a7be69ac74e6a7366873e0d02b60a1931ddea2
RauscherTasha <natascharauscher@gmx.net>
2019-04-27 18:25:55 +0200

    text klick
    next text on click on option text baiscs

--- js/SceneMainMenu.js
+++ js/SceneMainMenu.js
@@ -12,18 +12,43 @@ class SceneMainMenu extends Phaser.Scene {
 
     create() {
 
-        this.se = this.cache.json.get('SE');
-        this.index = 0;
-        let i =0
-        //for(i in ){
+        this.storyElemnets = this.cache.json.get('SE');
+        this.storyIndex = 1;
 
-        //}
-        console.log("se "+this.se.se[1].story);
-        //console.log("wut "+this.se[this.index].story);
+        this.story = this.add.text(this.game.config.width * 0.5, this.game.config.height*0.1, this.storyElemnets.se[this.storyIndex].story, {
+            fontFamily: 'monospace',
+            fontSize: 15,
+            //fontStyle: 'bold',
+            color: '#ffffff',
+            align: 'center',
+        });
+        this.story.setInteractive(new Phaser.Geom.Rectangle(0, 0, this.story.width, this.story.height), Phaser.Geom.Rectangle.Contains);
+
+        this.story.setOrigin(0.5);
+
+        //Option
+        this.optionNeg = this.add.text(this.game.config.width * 0.5, this.game.config.height*(0.1+Math.random()*.8), this.storyElemnets.se[this.storyIndex].optionNeg, {
+            fontFamily: 'monospace',
+            fontSize: 15,
+            fontStyle: 'bold',
+            color: '#ffffff',
+            align: 'center',
+        });
+        this.optionNeg.setInteractive(new Phaser.Geom.Rectangle(0, 0, this.story.width, this.story.height), Phaser.Geom.Rectangle.Contains);
+
+        this.optionNeg.setOrigin(0.5);
+
+        this.optionNeg.on("pointerdown", function() {
+            //if(this.storyIndex < this.storyElements.se.length) {
+                this.storyIndex++;
+            //}
+        }, this);
+
+       //console.log("se "+this.storyElemnets.se[1].story);
 
     }
 
     update(){
-
+        this.story.setText(this.storyElemnets.se[this.storyIndex].story);
     }
 }
