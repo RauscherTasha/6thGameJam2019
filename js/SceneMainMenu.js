7c2874c75ce5da0d3ea5759938d514d5342445b7
RauscherTasha <natascharauscher@gmx.net>
2019-04-28 02:57:17 +0200

    WIP with some improvements
    Updated json (BG paths/ story / OptionNeg); get the Bgs in as a array
    (this.bg[index]; find a way to cycle them through with depth); also
    update optionNeg and random placement

--- js/SceneMainMenu.js
+++ js/SceneMainMenu.js
@@ -1,58 +1,33 @@
 class SceneMainMenu extends Phaser.Scene {
     constructor() {
-        super({ key: "SceneMainMenu" });
+        super({key: "SceneMainMenu"});
     }
 
     preload() {
-		
-		this.storyIndex = 1;
-		
-		this.load.json('SE', 'assets/text.json'); // SE story elements
-		
-		this.load.image('background1', "assets/images/BGCrossroad.png");
-		this.load.image('background2', "assets/images/BGCrossroad2.png");
-		
-		
-		// this.load.start();
-		
-		
-
+        this.load.json('SE', 'assets/text.json'); // SE story elements
     }
 
     create() {
-		/*
-		this.load.on('start', function () {
-			// Your handler code
-			this.storyElements = this.cache.json.get('SE');
-			// this.storyIndex = 1;
-			// var story = this.storyElements.se[this.storyIndex].story;
-			var background_image = this.storyElements.se[this.storyIndex].background;
-			this.load.image('background', background_image);
-			
-			for (var i = 0; i < this.storyElements.se.length; i++) {
-				var background_image = this.storyElements.se[i].background;
-				this.load.image('background' + i, background_image);
-			}
-			
-		}, this);
-		*/
-		
-
-		/*
-		this.load.on('filecomplete', function (key, type, data) {
-					this.add.image(0, 0, 'background').setOrigin(0, 0);
-				}, this);
-				*/
-		
-		this.bg = this.add.image(0, 0, 'background1').setOrigin(0, 0);
-		this.bg.depth = 100;
-
-		
         this.storyElements = this.cache.json.get('SE');
-		
-		//this.add.image(0, 0, 'background').setOrigin(0, 0);
+        this.storyElementsLenght = this.storyElements.se.length;
+        this.storyIndex = 0;
+        this.bg = [];
 
-        this.story = this.add.text(this.game.config.width * 0.5, this.game.config.height*0.1, this.storyElements.se[this.storyIndex].story, {
+        for(let i=0; i<this.storyElementsLenght;i++){
+            this.load.image('background' + i, this.storyElements.se[i].background);
+            this.load.start();
+        }
+        this.load.once('filecomplete', addFiles, this);
+
+        function addFiles(){
+            for(let i=0; i<this.storyElementsLenght;i++) {
+
+                this.bg[i] = this.add.image(0, 0, 'background' + i).setOrigin(0, 0);
+            }
+            console.log("done");
+        }
+
+        this.story = this.add.text(this.game.config.width * 0.5, this.game.config.height * 0.1, this.storyElements.se[this.storyIndex].story, {
             fontFamily: 'monospace',
             fontSize: 40,
             fontStyle: 'bold',
@@ -64,7 +39,7 @@ class SceneMainMenu extends Phaser.Scene {
         this.story.setOrigin(0.5);
 
         //Option
-        this.optionNeg = this.add.text(this.game.config.width * 0.5, this.game.config.height*(0.1+Math.random()*.8), this.storyElements.se[this.storyIndex].optionNeg, {
+        this.optionNeg = this.add.text(this.game.config.width * 0.5, this.game.config.height * (0.1 + Math.random() * .8), this.storyElements.se[this.storyIndex].optionNeg, {
             fontFamily: 'monospace',
             fontSize: 40,
             fontStyle: 'bold',
@@ -75,27 +50,20 @@ class SceneMainMenu extends Phaser.Scene {
 
         this.optionNeg.setOrigin(0.5);
 
-        this.optionNeg.on("pointerdown", function() {
-            //if(this.storyIndex < this.storyElements.se.length) {
+        this.optionNeg.on("pointerdown", function () {
+            if(this.storyIndex < this.storyElementsLenght) {
+                this.bg[this.storyIndex].depth = 0;
+                console.log(this.bg);
                 this.storyIndex++;
-				this.story.setText(this.storyElements.se[this.storyIndex].story);
-				var old_depth = this.bg.depth;
-				this.bg = this.add.image(0, 0, 'background'+this.storyIndex).setOrigin(0, 0);
-				this.bg.depth = old_depth + 10;
-				this.story.depth = old_depth + 20;
+                this.story.setText(this.storyElements.se[this.storyIndex].story);
+                this.optionNeg.setText(this.storyElements.se[this.storyIndex].optionNeg);
+                this.optionNeg.setY(this.game.config.height * (0.1 + Math.random() * .8));
+                //this.bg = (this.add.image(0, 0, 'background' + this.storyIndex).setOrigin(0, 0));
 
-				/*
-				this.load.on('filecomplete', function (key, type, data) {
-					this.add.image(0, 0, 'background').setOrigin(0, 0);
-				}, this);
-				*/
-				// var background_image = this.storyElements.se[this.storyIndex].background;
-				// this.load.image('background', background_image);
-            //}
+            }
+
         }, this);
-		
-		this.story.depth = 1000;
-		this.optionNeg.depth = 2000;
+
 
     }
 
